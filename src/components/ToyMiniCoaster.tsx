17abb9e03252f0b4234bfba5bc4bb1196c34cea7
naotaro0123 <n.ikewada@gmail.com>
2024-03-24 17:41:53 +0900

    fix: add cannon

--- src/components/ToyMiniCoaster.tsx
+++ src/components/ToyMiniCoaster.tsx
@@ -1,3 +1,17 @@
+import { Physics, usePlane } from '@react-three/cannon';
+import { MeshProps } from '@react-three/fiber';
+import * as THREE from 'three';
+
+const StaticBoxMesh = (meshProps: MeshProps) => {
+  const { name, position, rotation } = meshProps;
+  const [ref] = usePlane<THREE.Mesh>(() => ({
+    type: 'Static',
+    position: position as [number, number, number],
+    rotation: rotation as [number, number, number],
+  }));
+  return <mesh ref={ref} name={name} args={meshProps.args}></mesh>;
+};
+
 export const ToyMiniCoaster = (): JSX.Element => {
   return (
     <>
@@ -5,106 +19,196 @@ export const ToyMiniCoaster = (): JSX.Element => {
         <directionalLight position={[5, 5, 3]} intensity={2} />
         <directionalLight position={[-5, -5, -3]} intensity={2} />
       </group>
-      <group name="mini-coaster">
-        <group name="bottom-wall" position={[0, -2, 0]}>
-          <mesh name="bottom-wall-base" position={[0, 0, 0]}>
-            <boxGeometry args={[4.2, 0.2, 1]} />
-            <meshStandardMaterial color={'orange'} />
-          </mesh>
-          <mesh name="bottom-wall-back" position={[0, 0.1, 0.55]}>
-            <boxGeometry args={[4.2, 0.4, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-          <mesh name="bottom-wall-front" position={[0, 0.1, -0.55]}>
-            <boxGeometry args={[4.2, 0.4, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-        </group>
 
-        <mesh name="left-wall" position={[2, 0.3, 0]} rotation={[0, 0, Math.PI / 2]}>
-          <boxGeometry args={[4.4, 0.2, 1]} />
-          <meshStandardMaterial color={'orange'} />
-        </mesh>
-        <mesh name="right-wall" position={[-2, 0.1, 0]} rotation={[0, 0, Math.PI / 2]}>
-          <boxGeometry args={[4, 0.2, 1]} />
-          <meshStandardMaterial color={'orange'} />
-        </mesh>
+      <Physics>
+        <group name="mini-coaster">
+          <group name="bottom-wall" position={[0, -2, 0]}>
+            <StaticBoxMesh
+              name="bottom-wall-base"
+              position={[0, 0, 0]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 1),
+                new THREE.MeshStandardMaterial({ color: 'orange' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="bottom-wall-back"
+              position={[0, 0.1, 0.55]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.4, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="bottom-wall-front"
+              position={[0, 0.1, -0.55]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.4, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+          </group>
 
-        <group name="top-wall" position={[0, 2.1, 0]} rotation={[0, 0, Math.PI / 24]}>
-          <mesh name="top-wall-base" position={[0.25, 0, 0]}>
-            <boxGeometry args={[3.3, 0.1, 0.7]} />
-            <meshStandardMaterial color={'orange'} />
-          </mesh>
-          <mesh name="top-wall-back" position={[0, 0, 0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-          <mesh name="top-wall-front" position={[0, 0, -0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-        </group>
+          <StaticBoxMesh
+            name="left-wall"
+            position={[2, 0.3, 0]}
+            rotation={[0, 0, Math.PI / 2]}
+            args={[
+              new THREE.BoxGeometry(4.4, 0.2, 1),
+              new THREE.MeshStandardMaterial({ color: 'orange' }),
+            ]}
+          ></StaticBoxMesh>
 
-        <group name="second-top-wall" position={[0, 1.2, 0]} rotation={[0, 0, -Math.PI / 24]}>
-          <mesh name="second-top-wall-base" position={[-0.25, 0, 0]}>
-            <boxGeometry args={[3.3, 0.1, 0.7]} />
-            <meshStandardMaterial color={'orange'} />
-          </mesh>
-          <mesh name="second-top-wall-back" position={[0, 0, 0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-          <mesh name="second-top-wall-front" position={[0, 0, -0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-        </group>
+          <StaticBoxMesh
+            name="right-wall"
+            position={[-2, 0.1, 0]}
+            rotation={[0, 0, Math.PI / 2]}
+            args={[
+              new THREE.BoxGeometry(4, 0.2, 1),
+              new THREE.MeshStandardMaterial({ color: 'orange' }),
+            ]}
+          ></StaticBoxMesh>
 
-        <group name="third-top-wall" position={[0, 0.2, 0]} rotation={[0, 0, Math.PI / 24]}>
-          <mesh name="third-top-wall-base" position={[0.25, 0, 0]}>
-            <boxGeometry args={[3.3, 0.1, 0.7]} />
-            <meshStandardMaterial color={'orange'} />
-          </mesh>
-          <mesh name="third-top-wall-back" position={[0, 0, 0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-          <mesh name="third-top-wall-front" position={[0, 0, -0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-        </group>
+          <group name="top-wall" position={[0, 2.1, 0]} rotation={[0, 0, Math.PI / 24]}>
+            <StaticBoxMesh
+              name="top-wall-base"
+              position={[0, 0, 0]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 1),
+                new THREE.MeshStandardMaterial({ color: 'orange' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="top-wall-back"
+              position={[0, 0, 0.55]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.4, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="top-wall-front"
+              position={[0, 0, -0.55]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.4, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+          </group>
 
-        <group name="fourth-top-wall" position={[0, -0.8, 0]} rotation={[0, 0, -Math.PI / 24]}>
-          <mesh name="fourth-top-wall-base" position={[-0.25, 0, 0]}>
-            <boxGeometry args={[3.3, 0.1, 0.7]} />
-            <meshStandardMaterial color={'orange'} />
-          </mesh>
-          <mesh name="fourth-top-wall-back" position={[0, 0, 0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-          <mesh name="fourth-top-wall-front" position={[0, 0, -0.4]}>
-            <boxGeometry args={[4.2, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-        </group>
+          <group name="second-top-wall" position={[0, 1.2, 0]} rotation={[0, 0, -Math.PI / 24]}>
+            <StaticBoxMesh
+              name="second-top-wall-base"
+              position={[-0.25, 0, 0]}
+              args={[
+                new THREE.BoxGeometry(3.3, 0.1, 0.7),
+                new THREE.MeshStandardMaterial({ color: 'orange' }),
+              ]}
+            ></StaticBoxMesh>
+            {/* <mesh name="second-top-wall-base" position={[-0.25, 0, 0]}>
+              <boxGeometry args={[3.3, 0.1, 0.7]} />
+              <meshStandardMaterial color={'orange'} />
+            </mesh> */}
+            <StaticBoxMesh
+              name="second-top-wall-back"
+              position={[0, 0, 0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="second-top-wall-front"
+              position={[0, 0, -0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+          </group>
 
-        <group name="fifth-top-wall" position={[0.28, -1.8, 0]} rotation={[0, 0, Math.PI / 24]}>
-          <mesh name="fifth-top-wall-base" position={[0, 0, 0]}>
-            <boxGeometry args={[3.3, 0.1, 0.7]} />
-            <meshStandardMaterial color={'orange'} />
-          </mesh>
-          <mesh name="fifth-top-wall-back" position={[0, 0, 0.4]}>
-            <boxGeometry args={[3.3, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
-          <mesh name="fifth-top-wall-front" position={[0, 0, -0.4]}>
-            <boxGeometry args={[3.3, 0.2, 0.1]} />
-            <meshStandardMaterial color={'peru'} />
-          </mesh>
+          <group name="third-top-wall" position={[0, 0.2, 0]} rotation={[0, 0, Math.PI / 24]}>
+            <StaticBoxMesh
+              name="third-top-wall-base"
+              position={[0, 0, 0]}
+              args={[
+                new THREE.BoxGeometry(3.3, 0.1, 0.7),
+                new THREE.MeshStandardMaterial({ color: 'orange' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="third-top-wall-back"
+              position={[0, 0, 0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="third-top-wall-front"
+              position={[0, 0, -0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+          </group>
+
+          <group name="fourth-top-wall" position={[0, -0.8, 0]} rotation={[0, 0, -Math.PI / 24]}>
+            <StaticBoxMesh
+              name="fourth-top-wall-base"
+              position={[-0.25, 0, 0]}
+              args={[
+                new THREE.BoxGeometry(3.3, 0.1, 0.7),
+                new THREE.MeshStandardMaterial({ color: 'orange' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="fourth-top-wall-back"
+              position={[0, 0, 0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="fourth-top-wall-front"
+              position={[0, 0, -0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+          </group>
+
+          <group name="fifth-top-wall" position={[0.28, -1.8, 0]} rotation={[0, 0, Math.PI / 24]}>
+            <StaticBoxMesh
+              name="fifth-top-wall-base"
+              position={[0, 0, 0]}
+              args={[
+                new THREE.BoxGeometry(3.3, 0.1, 0.7),
+                new THREE.MeshStandardMaterial({ color: 'orange' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="fifth-top-wall-back"
+              position={[0, 0, 0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+            <StaticBoxMesh
+              name="fifth-top-wall-front"
+              position={[0, 0, -0.4]}
+              args={[
+                new THREE.BoxGeometry(4.2, 0.2, 0.1),
+                new THREE.MeshStandardMaterial({ color: 'peru' }),
+              ]}
+            ></StaticBoxMesh>
+          </group>
         </group>
-      </group>
+      </Physics>
     </>
   );
 };
