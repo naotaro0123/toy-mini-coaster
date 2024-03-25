69966349abb0f8cbed8de93ff86f73a24902ee35
naotaro0123 <n.ikewada@gmail.com>
2024-03-25 22:15:22 +0900

    fix: change html button

--- src/components/HelloCube.tsx
+++ src/components/HelloCube.tsx
@@ -1,9 +1,17 @@
+import { Html } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
 import { useState } from 'react';
 
 type Item = { id: string; position: [number, number, number] };
 
 export const HelloCube = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const { size } = useThree();
+  // create button position
+  const buttonWidth = 80;
+  const buttonHeight = 40;
+  const htmlTop = -size.height / 2;
+  const htmlRight = -size.width / 2;
 
   return (
     <>
@@ -11,19 +19,36 @@ export const HelloCube = () => {
         <directionalLight position={[5, 5, 3]} intensity={2} />
         <directionalLight position={[-5, -5, -3]} intensity={2} />
       </group>
-      <mesh
-        position={[0, 0, 0]}
-        scale={[2, 2, 2]}
-        onClick={() =>
-          setItems([
-            ...items,
-            {
-              id: `item-${items.length + 1}`,
-              position: [Math.random() * 1.0, Math.random() * 3.0, 0],
-            },
-          ])
-        }
+      <Html
+        style={{
+          position: 'absolute',
+          top: htmlTop,
+          right: htmlRight,
+          margin: 10,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 10,
+        }}
       >
+        <button
+          style={{ width: buttonWidth, height: buttonHeight }}
+          onClick={() =>
+            setItems([
+              ...items,
+              {
+                id: `item-${items.length + 1}`,
+                position: [items.length + 1, 0, 0],
+              },
+            ])
+          }
+        >
+          create
+        </button>
+        <button style={{ width: buttonWidth, height: buttonHeight }} onClick={() => setItems([])}>
+          reset
+        </button>
+      </Html>
+      <mesh position={[0, 0, 0]} scale={[2, 2, 2]}>
         <boxGeometry args={[0.4, 0.4, 0.4]} />
         <meshStandardMaterial color={'orange'} />
       </mesh>
