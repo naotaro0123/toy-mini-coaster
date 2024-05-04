55521a32ce7d7d3faa6b2781a482d6d238c94cc3
naotaro0123 <n.ikewada@gmail.com>
2024-05-05 00:06:28 +0900

    fix: dropType

--- src/components/SimpleRapierPhysics.tsx
+++ src/components/SimpleRapierPhysics.tsx
@@ -5,6 +5,7 @@ import { Physics, RigidBody } from '@react-three/rapier';
 import * as THREE from 'three';
 
 type Item = { id: string; position: [number, number, number] };
+type DropType = 'ball' | 'car';
 
 const bodyLength = 12;
 const bodyWidth = 8;
@@ -38,12 +39,13 @@ export const SimpleRapierPhysics = () => {
   const [items, setItems] = useState<Item[]>(
     Array.from({ length: 1 }, (_, i) => ({
       id: `item-${i + 1}`,
-      position: [Math.random() * 4.0 - 2.0, 0, Math.random() * 2.0 - 1.0],
+      position: [5, 2, Math.random() * 2.0 - 1.0],
     })),
   );
+  const [dropType, setDropType] = useState<DropType>('ball');
   const { size } = useThree();
   // create button position
-  const buttonWidth = 80;
+  const buttonWidth = 130;
   const buttonHeight = 40;
   const htmlTop = -size.height / 2;
   const htmlRight = -size.width / 2;
@@ -72,7 +74,7 @@ export const SimpleRapierPhysics = () => {
               ...items,
               {
                 id: `item-${items.length + 1}`,
-                position: [Math.random() * 4.0 - 2.0, 0, Math.random() * 2.0 - 1.0],
+                position: [5, 2, Math.random() * 2.0 - 1.0],
               },
             ])
           }
@@ -82,17 +84,19 @@ export const SimpleRapierPhysics = () => {
         <button style={{ width: buttonWidth, height: buttonHeight }} onClick={() => setItems([])}>
           reset
         </button>
-        <button
-          style={{ width: buttonWidth, height: buttonHeight }}
-          onClick={() => console.log('items', items)}
-        >
-          debug
-        </button>
+        <div>
+          dropType:
+          <select value={dropType} onChange={(e) => setDropType(e.target.value as DropType)}>
+            <option value="ball">ball</option>
+            <option value="car">car</option>
+          </select>
+        </div>
       </Html>
 
       <Suspense>
         <Physics debug={true} colliders={false}>
           <RigidBody type="fixed" colliders="cuboid" rotation={[0, 0, Math.PI / 8]}>
+            {/* <RigidBody type="fixed" colliders="cuboid"> */}
             <mesh position={[0, -2, 0]}>
               <boxGeometry args={[10, 0.4, 4]} />
               <meshStandardMaterial color={'orange'} />
@@ -109,9 +113,9 @@ export const SimpleRapierPhysics = () => {
             </mesh>
           </RigidBody>
 
-          {items.map((item, i) => (
+          {items.map((item) => (
             <>
-              {i % 2 === 0 ? (
+              {dropType === 'ball' ? (
                 <RigidBody colliders="ball" position={item.position}>
                   <mesh>
                     <sphereGeometry args={[0.2, 10, 10]} />
@@ -126,25 +130,34 @@ export const SimpleRapierPhysics = () => {
                 //   </mesh>
                 // </RigidBody>
 
-                <RigidBody
-                  colliders="cuboid"
-                  position={item.position}
-                  rotation={[0, Math.PI / 2, 0]}
-                >
-                  <group>
-                    <mesh position={[0, 0, 0]} scale={0.1}>
-                      <extrudeGeometry args={[bodyShape, bodyExtrudeSettings]} />
-                      <meshStandardMaterial color={'blue'} />
-                    </mesh>
-                    {/* front-right */}
-                    <TireMesh position={[0.22, 0.4, -0.1]} />
-                    {/* front-left */}
-                    <TireMesh position={[0.98, 0.4, -0.1]} />
-                    {/* back-right */}
-                    <TireMesh position={[0.22, 0.4, 0.72]} />
-                    {/* back-left */}
-                    <TireMesh position={[0.98, 0.4, 0.72]} />
-                  </group>
+                // <RigidBody
+                //   colliders="cuboid"
+                //   position={item.position}
+                //   rotation={[0, Math.PI / 2, 0]}
+                // >
+                //   <group>
+                //     <mesh position={[0, 0, 0]} scale={0.1}>
+                //       <extrudeGeometry args={[bodyShape, bodyExtrudeSettings]} />
+                //       <meshStandardMaterial color={'blue'} />
+                //     </mesh>
+                //     {/* front-right */}
+                //     <TireMesh position={[0.22, 0.4, -0.1]} />
+                //     {/* front-left */}
+                //     <TireMesh position={[0.98, 0.4, -0.1]} />
+                //     {/* back-right */}
+                //     <TireMesh position={[0.22, 0.4, 0.72]} />
+                //     {/* back-left */}
+                //     <TireMesh position={[0.98, 0.4, 0.72]} />
+                //   </group>
+                // </RigidBody>
+
+                <RigidBody colliders="hull" position={item.position} rotation={[0, Math.PI / 2, 0]}>
+                  <mesh>
+                    <boxGeometry args={[1.0, 0.1, 0.1]} />
+                    <meshStandardMaterial color={'blue'} />
+                  </mesh>
+                  <TireMesh position={[0.5, 0, 0]} />
+                  <TireMesh position={[-0.5, 0, 0]} />
                 </RigidBody>
               )}
             </>
